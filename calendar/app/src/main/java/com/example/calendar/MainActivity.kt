5cfb3bfad41c780707d3bb9f7f11446242baf300
AbedMir31 <abedmir31@gmail.com>
2019-11-10 02:43:33 -0600

    Added button and constraints

--- calendar/app/src/main/java/com/example/calendar/MainActivity.kt
+++ calendar/app/src/main/java/com/example/calendar/MainActivity.kt
@@ -10,12 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val calendarView = findViewById<CalendarView>(R.id.calendarView)
-        calendarView?.setOnDateChangeListener { view, year, month, dayOfMonth ->
-            // Note that months are indexed from 0. So, 0 means January, 1 means february, 2 means march etc.
-            val msg = "Selected date is " + dayOfMonth + "/" + (month + 1) + "/" + year
-            Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
-        }
     }
 }
