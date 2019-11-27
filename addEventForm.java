e028731ad12398b138b7a1e102ce97b8e4e67f14
gelarehno73 <55035142+gelarehno73@users.noreply.github.com>
2019-11-27 11:07:35 -0600

    Add files via upload

---
+++ addEventForm.java
@@ -0,0 +1,14 @@
+package com.example.addeventbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addEventForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event_form);
+    }
+}
