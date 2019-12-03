f77cb9352b3852e54485a6385ca57b4ae89685b3
AbedMir31 <abedmir31@gmail.com>
2019-12-03 14:26:40 -0600

    added array and data format

--- calendar/app/src/main/java/com/example/calendar/Event.java
+++ calendar/app/src/main/java/com/example/calendar/Event.java
@@ -1,11 +1,65 @@
 package com.example.calendar;
 
-public class Event {
-    String Title;
-    int Year;
-    int Month;
-    int Day;
-    int Hour;
-    int Minute;
+import androidx.annotation.NonNull;
 
+public class Event {
+    private String Title;
+    private int Year, Month, Day, Hour, Minute;
+
+    Event(String t, int day, int month, int year, int hour, int minute){
+        this.Title = t;
+        this.Day = day;
+        this.Month = month;
+        this.Year = year;
+        this.Hour = hour;
+        this.Minute = minute;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public int getYear() {
+        return Year;
+    }
+
+    public void setYear(int year) {
+        Year = year;
+    }
+
+    public int getMonth() {
+        return Month;
+    }
+
+    public void setMonth(int month) {
+        Month = month;
+    }
+
+    public int getDay() {
+        return Day;
+    }
+
+    public void setDay(int day) {
+        Day = day;
+    }
+
+    public int getHour() {
+        return Hour;
+    }
+
+    public void setHour(int hour) {
+        Hour = hour;
+    }
+
+    public int getMinute() {
+        return Minute;
+    }
+
+    public void setMinute(int minute) {
+        Minute = minute;
+    }
 }
--- calendar/app/src/main/java/com/example/calendar/EventActivity.kt
+++ calendar/app/src/main/java/com/example/calendar/EventActivity.kt
@@ -23,7 +23,9 @@ class EventActivity : AppCompatActivity() {
     private class MyCustomAdapter(context: Context): BaseAdapter() {
 
         private val mContext : Context
-        private val eventList = arrayListOf<Event>()
+        private val eventList = arrayListOf<Event>(
+            Event("Engineering Final", 4, 3,2019, 5, 30)
+        )
 
         init {
             mContext = context
@@ -34,7 +36,7 @@ class EventActivity : AppCompatActivity() {
 
         //how many rows in the list
         override fun getCount(): Int {
-            return 7
+            return eventList.size
         }
 
         //Renders out each row
@@ -46,10 +48,12 @@ class EventActivity : AppCompatActivity() {
             val layoutInflator = LayoutInflater.from(mContext)
             val rowMain = layoutInflator.inflate(R.layout.activity_event_row, viewGroup, false)
 
+            val currentEvent = eventList[position]
             val nameTextView = rowMain.findViewById<TextView>(R.id.name_textview)
-            nameTextView.text = "Event Title"
+            nameTextView.text = currentEvent.title
+
             val positionTextView = rowMain.findViewById<TextView>(R.id.position_textview)
-            positionTextView.text = "Event Number: $position"
+            positionTextView.text = "Date: ${currentEvent.day}/${currentEvent.month}/${currentEvent.year}\t\t${currentEvent.hour}:${currentEvent.minute}"
             return rowMain
         }
     }
